dfc7e85f40dd5456eccbf913e2853242cf38fe8d
Timtech-wiz <ajetetimothysamuel@gmail.com>
2022-08-19 15:35:15 +0100

    add cbt data

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -9,14 +9,34 @@ defineProps({
 
 <template>
   <div class="greetings">
-    <h1 class="green">{{ msg }}</h1>
+    <!-- <h1 class="text-red-900 border-2 bg-gray-600 py-3 px-5 rounded-lg">{{ msg }}</h1>
     <h3>
       You’ve successfully created a project with
       <a target="_blank" href="https://vitejs.dev/">Vite</a> +
       <a target="_blank" href="https://vuejs.org/">Vue 3</a>.
-    </h3>
+    </h3> -->
+    {{questions}}
   </div>
 </template>
+
+<script>
+ 
+export default {
+  data(){
+    return{
+      questions:[]
+    }
+  },
+
+  mounted(){
+    fetch('http://localhost:3001/cbt')
+    .then(response => response.json())
+    .then(res => {
+      this.questions = res
+    })
+  }
+}
+</script>
 
 <style scoped>
 h1 {
