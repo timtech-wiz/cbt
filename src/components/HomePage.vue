dea21561c3612dfcefd6ddf9926096ea7489c3c0
Timtech-wiz <ajetetimothysamuel@gmail.com>
2022-08-22 14:14:59 +0100

    complete cbt UI

---
+++ src/components/HomePage.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="flex">
+    <div class="mt-24 ml-3 w-1/2 font-semibold p-3 text-gray-400">
+        <h1 class="text-4xl">Welcome to Bishop Cbt App</h1>
+        <p class="mt-6 text-xl">Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio, officia
+            dolore! Itaque, commodi dolorem. Voluptas perspiciatis 
+            vel minima tempora sed, deleniti dicta consequatur 
+            illum iusto corporis veniam eum doloremque totam.
+        </p>
+    </div>
+    <div class="mt-[30rem] mr-9 w-1/2 font-semibold flex items-center justify-end h-12 ">
+        
+         
+     <router-link to="/quiz" class="mr-9 text-xl border
+      border-gray-400 px-9 py-3 bg-blue-300 hover:bg-transparent 
+      hover:text-white
+      rounded-lg">Start</router-link>
+    </div>
+    <div>
+        
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
---
+++ src/components/partials/AppHeader.vue
@@ -0,0 +1,14 @@
+<template>
+    <div class="p-3 bg-gray-300">
+        <div class="flex justify-between">
+            <h1>CBT</h1>
+
+            <ul class="mr-6 text-xl flex border border-gray-900 bg-gray-700 text-white p-1 rounded-lg hover:bg-gray-800 hover:text-white">
+                <li>Time</li>
+                <p class="ml-2">00:34</p>
+            </ul>
+        </div>
+
+
+    </div>
+</template>
